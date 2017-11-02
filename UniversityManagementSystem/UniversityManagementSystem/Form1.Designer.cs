9b46b2e616edab83834c0f2e706732f9ecb26ce7
AnowerRony <hossainanower4@gmail.com>
2017-11-03 01:15:56 +0600

    ready page

---
+++ UniversityManagementSystem/UniversityManagementSystem/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace UniversityManagementSystem
+{
+    partial class startForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(startForm));
+            this.adminPictureBox = new System.Windows.Forms.PictureBox();
+            this.exitPictureBox = new System.Windows.Forms.PictureBox();
+            this.teacherPictureBox = new System.Windows.Forms.PictureBox();
+            this.studentPictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.adminPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exitPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teacherPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // adminPictureBox
+            // 
+            this.adminPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("adminPictureBox.Image")));
+            this.adminPictureBox.Location = new System.Drawing.Point(335, 75);
+            this.adminPictureBox.Name = "adminPictureBox";
+            this.adminPictureBox.Size = new System.Drawing.Size(140, 127);
+            this.adminPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.adminPictureBox.TabIndex = 0;
+            this.adminPictureBox.TabStop = false;
+            this.adminPictureBox.MouseLeave += new System.EventHandler(this.adminPictureBox_MouseLeave);
+            this.adminPictureBox.MouseHover += new System.EventHandler(this.adminPictureBox_MouseHover);
+            // 
+            // exitPictureBox
+            // 
+            this.exitPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("exitPictureBox.Image")));
+            this.exitPictureBox.Location = new System.Drawing.Point(781, 12);
+            this.exitPictureBox.Name = "exitPictureBox";
+            this.exitPictureBox.Size = new System.Drawing.Size(34, 28);
+            this.exitPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.exitPictureBox.TabIndex = 1;
+            this.exitPictureBox.TabStop = false;
+            this.exitPictureBox.MouseLeave += new System.EventHandler(this.exitPictureBox_MouseLeave);
+            this.exitPictureBox.MouseHover += new System.EventHandler(this.exitPictureBox_MouseHover);
+            // 
+            // teacherPictureBox
+            // 
+            this.teacherPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("teacherPictureBox.Image")));
+            this.teacherPictureBox.Location = new System.Drawing.Point(66, 348);
+            this.teacherPictureBox.Name = "teacherPictureBox";
+            this.teacherPictureBox.Size = new System.Drawing.Size(140, 127);
+            this.teacherPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.teacherPictureBox.TabIndex = 2;
+            this.teacherPictureBox.TabStop = false;
+            this.teacherPictureBox.MouseLeave += new System.EventHandler(this.teacherPictureBox_MouseLeave);
+            this.teacherPictureBox.MouseHover += new System.EventHandler(this.teacherPictureBox_MouseHover);
+            // 
+            // studentPictureBox
+            // 
+            this.studentPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("studentPictureBox.Image")));
+            this.studentPictureBox.Location = new System.Drawing.Point(619, 348);
+            this.studentPictureBox.Name = "studentPictureBox";
+            this.studentPictureBox.Size = new System.Drawing.Size(140, 127);
+            this.studentPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.studentPictureBox.TabIndex = 3;
+            this.studentPictureBox.TabStop = false;
+            this.studentPictureBox.MouseLeave += new System.EventHandler(this.studentPictureBox_MouseLeave);
+            this.studentPictureBox.MouseHover += new System.EventHandler(this.studentPictureBox_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(103, 325);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Teacher";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(377, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Admin";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(657, 325);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Student";
+            // 
+            // startForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(827, 525);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.studentPictureBox);
+            this.Controls.Add(this.teacherPictureBox);
+            this.Controls.Add(this.exitPictureBox);
+            this.Controls.Add(this.adminPictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "startForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.adminPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exitPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teacherPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox adminPictureBox;
+        private System.Windows.Forms.PictureBox exitPictureBox;
+        private System.Windows.Forms.PictureBox teacherPictureBox;
+        private System.Windows.Forms.PictureBox studentPictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ UniversityManagementSystem/UniversityManagementSystem/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UniversityManagementSystem.Properties;
+
+namespace UniversityManagementSystem
+{
+    public partial class startForm : Form
+    {
+        public startForm()
+        {
+            InitializeComponent();
+        }
+
+        private void adminPictureBox_MouseHover(object sender, EventArgs e)
+        {
+            adminPictureBox.Image = Resources.admin1;
+        }
+
+        private void adminPictureBox_MouseLeave(object sender, EventArgs e)
+        {
+            adminPictureBox.Image = Resources.admin;
+        }
+
+        private void teacherPictureBox_MouseHover(object sender, EventArgs e)
+        {
+            teacherPictureBox.Image = Resources.professor;
+        }
+
+        private void teacherPictureBox_MouseLeave(object sender, EventArgs e)
+        {
+            teacherPictureBox.Image = Resources.professor1;
+        }
+
+        private void studentPictureBox_MouseHover(object sender, EventArgs e)
+        {
+            studentPictureBox.Image = Resources.graduate1;
+        }
+
+        private void studentPictureBox_MouseLeave(object sender, EventArgs e)
+        {
+            studentPictureBox.Image = Resources.graduate;
+        }
+
+        private void exitPictureBox_MouseHover(object sender, EventArgs e)
+        {
+            exitPictureBox.Image = Resources.icon1;
+        }
+
+        private void exitPictureBox_MouseLeave(object sender, EventArgs e)
+        {
+            exitPictureBox.Image = Resources.icon;
+        }
+    }
+}
