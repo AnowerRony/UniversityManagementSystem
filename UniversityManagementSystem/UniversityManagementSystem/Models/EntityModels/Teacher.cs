f49f86b8de0088ddfff48ce46ad81c3f5ade197d
AnowerRony <hossainanower4@gmail.com>
2017-11-08 11:12:04 +0600

    department designation class

--- UniversityManagementSystem/UniversityManagementSystem/Models/EntityModels/Teacher.cs
+++ UniversityManagementSystem/UniversityManagementSystem/Models/EntityModels/Teacher.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace UniversityManagementSystem.Models.EntityModels
 {
-    class Teacher
+    public class Teacher
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string ContactNo { get; set; }
+        public string Address { get; set; }
+        public int DesignationId { get; set; }
+        public int DepartmentId { get; set; }
+
     }
 }
