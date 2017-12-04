b24391da002a2c7665f74b46298f6f3e1084ae8c
AnowerRony <hossainanower4@gmail.com>
2017-12-04 21:56:42 +0600

    ok

--- UniversityManagementSystem/UniversityManagementSystem/AdminPanel.Designer.cs
+++ UniversityManagementSystem/UniversityManagementSystem/AdminPanel.Designer.cs
@@ -28,11 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminPanel));
+            this.exitPictureBox = new System.Windows.Forms.PictureBox();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.logOutLabel = new System.Windows.Forms.Label();
+            this.saveDepartmentLabel = new System.Windows.Forms.Label();
+            this.viewDepartmentLabel = new System.Windows.Forms.Label();
+            this.saveTeacherLabel = new System.Windows.Forms.Label();
+            this.saveCourseLabel = new System.Windows.Forms.Label();
+            this.assignCourseLabel = new System.Windows.Forms.Label();
+            this.allocateClassroomLabel = new System.Windows.Forms.Label();
+            this.viewCourseLabel = new System.Windows.Forms.Label();
+            this.viewRoomLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.exitPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // exitPictureBox
+            // 
+            this.exitPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("exitPictureBox.Image")));
+            this.exitPictureBox.Location = new System.Drawing.Point(950, 2);
+            this.exitPictureBox.Name = "exitPictureBox";
+            this.exitPictureBox.Size = new System.Drawing.Size(28, 28);
+            this.exitPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.exitPictureBox.TabIndex = 0;
+            this.exitPictureBox.TabStop = false;
+            this.exitPictureBox.Click += new System.EventHandler(this.exitPictureBox_Click);
+            this.exitPictureBox.MouseLeave += new System.EventHandler(this.exitPictureBox_MouseLeave);
+            this.exitPictureBox.MouseHover += new System.EventHandler(this.exitPictureBox_MouseHover);
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userNameLabel.ForeColor = System.Drawing.Color.Blue;
+            this.userNameLabel.Location = new System.Drawing.Point(54, 29);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(0, 20);
+            this.userNameLabel.TabIndex = 1;
+            // 
+            // logOutLabel
+            // 
+            this.logOutLabel.AutoSize = true;
+            this.logOutLabel.BackColor = System.Drawing.Color.RoyalBlue;
+            this.logOutLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logOutLabel.ForeColor = System.Drawing.Color.White;
+            this.logOutLabel.Location = new System.Drawing.Point(12, 6);
+            this.logOutLabel.Name = "logOutLabel";
+            this.logOutLabel.Size = new System.Drawing.Size(105, 25);
+            this.logOutLabel.TabIndex = 3;
+            this.logOutLabel.Text = "LOG OUT";
+            this.logOutLabel.Click += new System.EventHandler(this.logOutLabel_Click);
+            // 
+            // saveDepartmentLabel
+            // 
+            this.saveDepartmentLabel.AutoSize = true;
+            this.saveDepartmentLabel.BackColor = System.Drawing.Color.DodgerBlue;
+            this.saveDepartmentLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveDepartmentLabel.ForeColor = System.Drawing.Color.White;
+            this.saveDepartmentLabel.Location = new System.Drawing.Point(53, 120);
+            this.saveDepartmentLabel.Name = "saveDepartmentLabel";
+            this.saveDepartmentLabel.Size = new System.Drawing.Size(178, 25);
+            this.saveDepartmentLabel.TabIndex = 4;
+            this.saveDepartmentLabel.Text = "Save Department";
+            // 
+            // viewDepartmentLabel
+            // 
+            this.viewDepartmentLabel.AutoSize = true;
+            this.viewDepartmentLabel.BackColor = System.Drawing.Color.DodgerBlue;
+            this.viewDepartmentLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewDepartmentLabel.ForeColor = System.Drawing.Color.White;
+            this.viewDepartmentLabel.Location = new System.Drawing.Point(310, 120);
+            this.viewDepartmentLabel.Name = "viewDepartmentLabel";
+            this.viewDepartmentLabel.Size = new System.Drawing.Size(175, 25);
+            this.viewDepartmentLabel.TabIndex = 4;
+            this.viewDepartmentLabel.Text = "View Department";
+            // 
+            // saveTeacherLabel
+            // 
+            this.saveTeacherLabel.AutoSize = true;
+            this.saveTeacherLabel.BackColor = System.Drawing.Color.DodgerBlue;
+            this.saveTeacherLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveTeacherLabel.ForeColor = System.Drawing.Color.White;
+            this.saveTeacherLabel.Location = new System.Drawing.Point(53, 177);
+            this.saveTeacherLabel.Name = "saveTeacherLabel";
+            this.saveTeacherLabel.Size = new System.Drawing.Size(146, 25);
+            this.saveTeacherLabel.TabIndex = 4;
+            this.saveTeacherLabel.Text = "Save Teacher";
+            // 
+            // saveCourseLabel
+            // 
+            this.saveCourseLabel.AutoSize = true;
+            this.saveCourseLabel.BackColor = System.Drawing.Color.DodgerBlue;
+            this.saveCourseLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveCourseLabel.ForeColor = System.Drawing.Color.White;
+            this.saveCourseLabel.Location = new System.Drawing.Point(53, 231);
+            this.saveCourseLabel.Name = "saveCourseLabel";
+            this.saveCourseLabel.Size = new System.Drawing.Size(136, 25);
+            this.saveCourseLabel.TabIndex = 4;
+            this.saveCourseLabel.Text = "Save Course";
+            // 
+            // assignCourseLabel
+            // 
+            this.assignCourseLabel.AutoSize = true;
+            this.assignCourseLabel.BackColor = System.Drawing.Color.DodgerBlue;
+            this.assignCourseLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.assignCourseLabel.ForeColor = System.Drawing.Color.White;
+            this.assignCourseLabel.Location = new System.Drawing.Point(53, 285);
+            this.assignCourseLabel.Name = "assignCourseLabel";
+            this.assignCourseLabel.Size = new System.Drawing.Size(152, 25);
+            this.assignCourseLabel.TabIndex = 4;
+            this.assignCourseLabel.Text = "Assign Course";
+            // 
+            // allocateClassroomLabel
+            // 
+            this.allocateClassroomLabel.AutoSize = true;
+            this.allocateClassroomLabel.BackColor = System.Drawing.Color.DodgerBlue;
+            this.allocateClassroomLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.allocateClassroomLabel.ForeColor = System.Drawing.Color.White;
+            this.allocateClassroomLabel.Location = new System.Drawing.Point(53, 345);
+            this.allocateClassroomLabel.Name = "allocateClassroomLabel";
+            this.allocateClassroomLabel.Size = new System.Drawing.Size(197, 25);
+            this.allocateClassroomLabel.TabIndex = 4;
+            this.allocateClassroomLabel.Text = "Allocate Classroom";
+            // 
+            // viewCourseLabel
+            // 
+            this.viewCourseLabel.AutoSize = true;
+            this.viewCourseLabel.BackColor = System.Drawing.Color.DodgerBlue;
+            this.viewCourseLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewCourseLabel.ForeColor = System.Drawing.Color.White;
+            this.viewCourseLabel.Location = new System.Drawing.Point(310, 231);
+            this.viewCourseLabel.Name = "viewCourseLabel";
+            this.viewCourseLabel.Size = new System.Drawing.Size(226, 25);
+            this.viewCourseLabel.TabIndex = 4;
+            this.viewCourseLabel.Text = "View Course Statistics";
+            // 
+            // viewRoomLabel
+            // 
+            this.viewRoomLabel.AutoSize = true;
+            this.viewRoomLabel.BackColor = System.Drawing.Color.DodgerBlue;
+            this.viewRoomLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewRoomLabel.ForeColor = System.Drawing.Color.White;
+            this.viewRoomLabel.Location = new System.Drawing.Point(310, 177);
+            this.viewRoomLabel.Name = "viewRoomLabel";
+            this.viewRoomLabel.Size = new System.Drawing.Size(216, 25);
+            this.viewRoomLabel.TabIndex = 4;
+            this.viewRoomLabel.Text = "View Room Schedule";
+            // 
+            // AdminPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(983, 569);
+            this.Controls.Add(this.viewCourseLabel);
+            this.Controls.Add(this.allocateClassroomLabel);
+            this.Controls.Add(this.assignCourseLabel);
+            this.Controls.Add(this.saveCourseLabel);
+            this.Controls.Add(this.saveTeacherLabel);
+            this.Controls.Add(this.viewRoomLabel);
+            this.Controls.Add(this.viewDepartmentLabel);
+            this.Controls.Add(this.saveDepartmentLabel);
+            this.Controls.Add(this.logOutLabel);
+            this.Controls.Add(this.userNameLabel);
+            this.Controls.Add(this.exitPictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AdminPanel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AdminPanel";
+            ((System.ComponentModel.ISupportInitialize)(this.exitPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox exitPictureBox;
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.Label logOutLabel;
+        private System.Windows.Forms.Label saveDepartmentLabel;
+        private System.Windows.Forms.Label viewDepartmentLabel;
+        private System.Windows.Forms.Label saveTeacherLabel;
+        private System.Windows.Forms.Label saveCourseLabel;
+        private System.Windows.Forms.Label assignCourseLabel;
+        private System.Windows.Forms.Label allocateClassroomLabel;
+        private System.Windows.Forms.Label viewCourseLabel;
+        private System.Windows.Forms.Label viewRoomLabel;
     }
 }
--- UniversityManagementSystem/UniversityManagementSystem/AdminPanel.cs
+++ UniversityManagementSystem/UniversityManagementSystem/AdminPanel.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UniversityManagementSystem.Properties;
 
 namespace UniversityManagementSystem
 {
@@ -16,5 +17,37 @@ namespace UniversityManagementSystem
         {
             InitializeComponent();
         }
+
+        private void exitPictureBox_MouseHover(object sender, EventArgs e)
+        {
+            exitPictureBox.Image = Resources.icon1;
+        }
+
+        private void exitPictureBox_MouseLeave(object sender, EventArgs e)
+        {
+            exitPictureBox.Image = Resources.icon;
+        }
+
+        private void exitPictureBox_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Do you really want to close the program?", "Exit",
+                MessageBoxButtons.YesNo);
+            if (dialog == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if (dialog == DialogResult.No)
+            {
+
+            }
+        }
+
+        private void logOutLabel_Click(object sender, EventArgs e)
+        {
+            AdminLogInForm openForm = new AdminLogInForm();
+            openForm.Show();
+            Visible = false;
+        }
+
     }
 }
